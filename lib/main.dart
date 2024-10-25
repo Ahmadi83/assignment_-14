b9fbec8d0cd980ed6c93f56f9f86378ad156b1ad
AHMADI <abdulrazeqahmaddi@gmail.com>
2024-10-25 17:06:02 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'List_Page.dart';
+import 'package:flutter/material.dart';
+import 'Add_Sales.dart';
+
+void main(){
+
+ runApp(MaterialApp(
+   home: Add_Sales(),
+ )) ;
+}
+
+
